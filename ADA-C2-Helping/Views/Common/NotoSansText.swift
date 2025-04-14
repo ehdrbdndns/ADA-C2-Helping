c3e5656d97bc44b1c31fc7be3a785d0561b1452d
ehdrbdndns <ehdrbdndns@naver.com>
2025-04-14 16:07:58 +0900

    custom font component 제작

---
+++ ADA-C2-Helping/Views/Common/NotoSansText.swift
@@ -0,0 +1,47 @@
+//
+//  NotoSansFont.swift
+//  ADA-C2-Helping
+//
+//  Created by Donggyun Yang on 4/13/25.
+//
+
+import SwiftUI
+
+enum NotoSansFontStyle: String {
+    case regular = "NotoSansKR-Regular"
+    case medium = "NotoSansKR-Medium"
+    case bold = "NotoSansKR-Bold"
+}
+
+struct NotoSansStyle {
+    let fontStyle: NotoSansFontStyle
+    let size: CGFloat
+    let color: Color
+    
+    init(fontStyle: NotoSansFontStyle = .regular, size: CGFloat = 17, color: Color = .black) {
+        self.fontStyle = fontStyle
+        self.size = size
+        self.color = color
+    }
+}
+
+struct NotoSansText: View {
+    var text: String
+    var style: NotoSansStyle
+    
+    init(_ text: String, _ style: NotoSansStyle = NotoSansStyle()) {
+        self.text = text
+        self.style = style
+    }
+    
+    var body: some View {
+        Text(text)
+            .font(Font.custom(style.fontStyle.rawValue, size: style.size))
+            .foregroundColor(style.color)
+    }
+}
+
+#Preview {
+    var textStyle = NotoSansStyle(fontStyle: .medium, size: 24, color: .black)
+    NotoSansText("hello", textStyle);
+}
