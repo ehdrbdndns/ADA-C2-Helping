9e2bc5a410b7929b350ddd8ed76e5f90e6fca0a6
ehdrbdndns <ehdrbdndns@naver.com>
2025-04-17 16:55:15 +0900

    add myView UI

--- ADA-C2-Helping/Views/MyView.swift
+++ ADA-C2-Helping/Views/MyView.swift
@@ -7,12 +7,72 @@
 
 import SwiftUI
 
-struct MyView: View {
+enum TabMode {
+    case myHelp
+    case giveHelp
+}
+
+struct Tab: View {
+    @Binding var tabMode: TabMode
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack() {
+            Button {
+                tabMode = .myHelp
+            } label: {
+                NotoSansText(
+                    "내가 작성한 글"
+                    , NotoSansStyle(fontStyle: .bold, size: 17, color: tabMode == .myHelp ? .black : .customGray)
+                )
+                .background(
+                    tabMode == .myHelp ?
+                        GeometryReader { geometry in
+                            Rectangle()
+                                .fill(.accent)
+                                .frame(width: geometry.size.width, height: 2)
+                                .offset(y: geometry.size.height + 4)
+                        } : nil
+                )
+            }
+            .frame(maxWidth: 180)
+            
+            Spacer()
+            
+            Button {
+                tabMode = .giveHelp
+            } label: {
+                NotoSansText(
+                    "도움을 주고 있어요"
+                    , NotoSansStyle(fontStyle: .bold, size: 17, color: tabMode == .giveHelp ? .black : .customGray)
+                )
+                .background(
+                    tabMode == .giveHelp ?
+                        GeometryReader { geometry in
+                            Rectangle()
+                                .fill(.accent)
+                                .frame(width: geometry.size.width, height: 2)
+                                .offset(y: geometry.size.height + 4)
+                        } : nil
+                )
+            }
+            .frame(maxWidth: 180)
+        }
     }
 }
 
-#Preview {
-    MyView()
+struct MyView: View {
+    @State private var tabMode: TabMode = .myHelp
+    @Binding var cards: [CardModel]
+    
+    var body: some View {
+        VStack {
+            // TODO: Tab
+            Tab(tabMode: $tabMode)
+                .padding(.top, 24)
+            
+            // TODO: List
+            CardList(cardModels: cards)
+        }
+        .padding(.horizontal, 16)
+    }
 }
