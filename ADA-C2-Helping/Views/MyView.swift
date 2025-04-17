90910ec4be23a8e56785a19774fdddfa81faa94f
ehdrbdndns <ehdrbdndns@naver.com>
2025-04-17 16:55:01 +0900

    add color asset

---
+++ ADA-C2-Helping/Views/MyView.swift
@@ -0,0 +1,18 @@
+//
+//  MyView.swift
+//  ADA-C2-Helping
+//
+//  Created by Donggyun Yang on 4/17/25.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyView()
+}
